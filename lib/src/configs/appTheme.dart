005a41378e90ca3d8209f15b11cfd85e6ad68c63
Ramjan75 <ramjanplus5@gmail.com>
2021-09-12 17:20:56 +0600

    cng

--- lib/src/configs/appTheme.dart
+++ lib/src/configs/appTheme.dart
@@ -7,7 +7,6 @@ class AppTheme {
   static Color textColor = HexColor('#323643');
   static Color textColor2 = HexColor('#BCBCBC');
   static Color iconColor = HexColor('#F7D107');
-  
 
   static ThemeData appTheme = ThemeData(
     fontFamily: 'Poppins Regular',
@@ -18,28 +17,6 @@ class AppTheme {
       iconTheme: IconThemeData(
         color: Colors.black,
       ),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      errorStyle: TextStyle(
-        color: Colors.red,
-      ),
-      contentPadding: EdgeInsets.symmetric(vertical: 12),
-      border: OutlineInputBorder(
-        borderSide: BorderSide.none,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20),
-        borderSide: BorderSide(
-          color: AppTheme.primaryColor,
-        ),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20),
-        borderSide: BorderSide(color: AppTheme.primaryColor),
-      ),
-      fillColor: Colors.white,
-      filled: true,
     ),
   );
 }
